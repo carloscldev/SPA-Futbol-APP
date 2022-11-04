ea66cf14074d2d0769a8fcdaac9d304b9685cfee
carloscldev <96172647+carloscldev@users.noreply.github.com>
2022-11-04 10:30:19 +0100

    Update

--- Proyecto-React/SPA-futbol-app/src/componentes/Footer.js
+++ Proyecto-React/SPA-futbol-app/src/componentes/Footer.js
@@ -4,7 +4,7 @@ export default class Footer extends React.Component {
   render() {
     return (
       <>
-        <div className="row bg-light fixed-bottom px-4 py-2">
+        <div className="row bg-light px-4 py-2">
           <div class="col-md-12 text-center py-3">
             <span className="ms-1"> Created with ♥ for Lumetrio </span>
             <span>&copy; Copyright 2022</span>
